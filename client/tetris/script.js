e7b83d375495530f13300bbf6ac130becda1f092
annebezwada <rsps24@gmail.com>
2022-02-17 11:50:11 -0500

    commit

---
+++ client/tetris/script.js
@@ -0,0 +1,10 @@
+// Configuration
+const squareCount = 200;
+const width = 10;
+const displayWidth = 4;
+const lTetromino = [
+  [1, width + 1, width * 2 + 1, 2],
+  [width, width + 1, width + 2, width * 2 + 2],
+  [1, width + 1, width * 2 + 1, width * 2],
+  [width, width * 2, width * 2 + 1, width * 2 + 2]
+];
