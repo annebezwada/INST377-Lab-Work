90b3913c9b8699bcefb4b97ec309b90fa59c5c66
annebezwada <rsps24@gmail.com>
2022-03-13 12:55:27 -0400

    added array

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -1,19 +1,24 @@
 // As the last step of your lab, hook this up to index.html
 
-async function mainEvent() {
-  // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // change this selector to match the id or classname of your actual form
-  form.addEventListener('submit', async (submitEvent) => {
-    // async has to be declared all the way to get an await
+async function mainEvent() { // the async keyword means we can make API requests
+  console.log('form submission'); // this is substituting for a "breakpoint"
+  const form = document.querySelector('.main_form');
+  const submit = documentquerySelector('.submit_button');
+  submit.style.display = 'none'; // it's better not to display this until the data has loaded
 
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-  });
+  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  if (arrayFromJson.data.length > 0) {
+    form.addEventListener('submit', async (submitEvent) => {
+      submitEvent.preventDefault();
+      console.log('form submission');
+      submit.style.display = 'block';
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+
+      dataHandler(arrayFromJson.data);
+    });
+  }
 }
 
 // this actually runs first! It's calling the function above
