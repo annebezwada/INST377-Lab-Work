295a5c18aecbf2513819b98c217f503a9307cf30
annebezwada <rsps24@gmail.com>
2022-04-09 11:51:58 -0400

    more progress on js

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -18,3 +18,32 @@ function restoArrayMake(dataArray) {
       console.log('range item', item);
     }); */
 }
+function createHtmlList(collection) {
+  // console.table(collection);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const { name } = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
+function initMap() {
+  const map = L.map('map').setView([51.505, -0.09], 13); // lat long zoom
+  L.tileLayer(
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+    }
+  ).addTo(map);
+  return map;
+}
