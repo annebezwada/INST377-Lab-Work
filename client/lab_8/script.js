c48a43a589257da98c3c527b200cf5c3c13e9e19
annebezwada <rsps24@gmail.com>
2022-04-10 18:45:52 -0400

    new changes added

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -15,9 +15,10 @@ function restoArrayMake(dataArray) {
   });
   return listItems;
   /* range.forEach((item) => {
-      console.log('range item', item);
-    }); */
+    console.log('range item', item);
+  }); */
 }
+
 function createHtmlList(collection) {
   // console.table(collection);
   const targetList = document.querySelector('.resto-list');
@@ -30,8 +31,9 @@ function createHtmlList(collection) {
   });
 }
 
-function initMap() {
-  const map = L.map('map').setView([51.505, -0.09], 13); // lat long zoom
+function initMap(targetID) {
+  const latLong = [38.784, -76.872];
+  const map = L.map(targetID).setView(latLong, 13); // lat long zoom
   L.tileLayer(
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
     {
@@ -47,6 +49,43 @@ function initMap() {
   ).addTo(map);
   return map;
 }
+
+function addMapMarkers(map, collection) {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+  collection.forEach((item) => {
+    const point = item.geocoded_column_1?.coordinates;
+    console.log(item.geocoded_column_1?.coordinates);
+    L.marker([point[1], point[0]]).addTo(map);
+  });
+}
+
+function refreshList(target, storage) {
+  target.addEventListener('click', async (event) => {
+    event.preventDefault();
+    localStorage.clear();
+    const results = await fetch('/api/foodServicesPG');
+    const arrayFromJson = await results.json();
+    console.log(arrayFromJson);
+    localStorage.setItem(storage, JSON.stringify(arrayFromJson.data));
+    location.reload();
+  });
+}
+// function inputListener(target) {
+//   target.addEventListener('input',async (event) => {
+//     console.log(event.target.value);
+//     const selectResto = storedDataArray.filter((item) => {
+//       const lowerName = item.name.toLowerCase();
+//       const lowerValue = event.target.value.toLowerCase();
+//       return lowerName.includes(lowerValue);
+//     });
+//     console.log(selectResto);
+//     createHtmlList(selectResto);
+//   });
+// }
 async function mainEvent() {
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // change this selector to match the id or classname of your actual form
@@ -54,11 +93,19 @@ async function mainEvent() {
 
   const resto = document.querySelector('#resto_name');
   const zipcode = document.querySelector('#zipcode');
-  const map = initMap();
+  const refresh = document.querySelector('#refresh_list');
+
+  const map = initMap('map');
+  const retrievalVar = 'restaurants';
   submit.style.display = 'none';
 
-  const arrayFromJson = { data: [] };
-  if (arrayFromJson.data.length > 0) {
+  refreshList(refresh, retrievalVar);
+
+  const storedDataString = localStorage.getItem(retrievalVar);
+  const storedDataArray = JSON.parse(storedDataString);
+  console.log(storedDataArray);
+  if (storedDataArray?.length > 0) {
+    // this statement is to prevent a race condition on data load
     submit.style.display = 'block';
 
     let currentArray = [];
@@ -90,14 +137,15 @@ async function mainEvent() {
       console.log(selectResto);
       createHtmlList(selectResto);
     });
-
+    // inputListener(resto);
     form.addEventListener('submit', async (submitEvent) => {
       // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       // console.log('form submission'); // this is substituting for a "breakpoint"
-      currentArray = restoArrayMake(arrayFromJson.data);
+      currentArray = restoArrayMake(storedDataArray);
       console.log(currentArray);
       createHtmlList(currentArray);
+      addMapMarkers(map, currentArray);
     });
   }
 }
