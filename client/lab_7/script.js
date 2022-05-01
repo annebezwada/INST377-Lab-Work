7e37e5c979b4487f8251f645160f273374f5c71b
annebezwada <rsps24@gmail.com>
2022-05-01 15:47:40 -0400

    fixing up lab 7

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,23 +1,95 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 // As the last step of your lab, hook this up to index.html
+function restoArrayMake(dataArray) {
+  // console.log('fired datahandler');
+  // console.table(dataArray);
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+  return listItems;
+  /* range.forEach((item) => {
+    console.log('range item', item);
+  }); */
+}
+
+function createHtmlList(collection) {
+  // console.table(collection);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
 
 async function mainEvent() {
   // the async keyword means we can make API requests
-  console.log('form submission'); // this is substituting for a "breakpoint"
-  const form = document.querySelector('.main_form');
-  const submit = documentquerySelector('.submit_button');
-  submit.style.display = 'none'; // it's better not to display this until the data has loaded
+  const form = document.querySelector('.main_form'); // change this selector to match the id or classname of your actual form
+  const submit = document.querySelector('.submit_button');
 
-  const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+  const resto = document.querySelector('#resto_name');
+  const zipcode = document.querySelector('#zipcode');
+
+  submit.style.display = 'none';
+
+  const results = await fetch(
+    '/api/foodServicesPG'
+  ); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  if (arrayFromJson.data.length > 0) {
-    form.addEventListener('submit', async (submitEvent) => {
-      submitEvent.preventDefault();
-      console.log('form submission');
-      submit.style.display = 'block';
-      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-      // it contains all 1,000 records we need
 
-      dataHandler(arrayFromJson.data);
+  // console.table(arrayFromJson.data); // this is called "dot notation"
+  // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+  // it contains all 1,000 records we need
+  if (arrayFromJson.data.length > 0) {
+    // this statement is to prevent a race condition on data load
+    submit.style.display = 'block';
+
+    let currentArray = [];
+    resto.addEventListener('input', async (event) => {
+      console.log(event.target.value);
+
+      if (currentArray.length < 1) {
+        return;
+      }
+      const selectResto = currentArray.filter((item) => {
+        const lowerName = item.name.toLowerCase();
+        const lowerValue = event.target.value.toLowerCase();
+        return lowerName.includes(lowerValue);
+      });
+      console.log(selectResto);
+      createHtmlList(selectResto);
+    });
+
+    zipcode.addEventListener('input', async(event) => {
+      console.log(event.target.value);
+      if (currentArray.length < 1) {
+        return;
+      }
+      const selectResto = currentArray.filter((item) => {
+        const lowerzip = item.zip.toLowerCase();
+        const lowerValue = event.target.value.toLowerCase();
+        return lowerzip.includes(lowerValue);
+      });
+      console.log(selectResto);
+      createHtmlList(selectResto);
+    });
+
+    form.addEventListener('submit', async (submitEvent) => {
+      // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      // console.log('form submission'); // this is substituting for a "breakpoint"
+      currentArray = restoArrayMake(arrayFromJson.data);
+      console.log(currentArray);
+      createHtmlList(currentArray);
     });
   }
 }
